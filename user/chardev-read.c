403867de828749db092d50db0252c8197a7e6a34
ByJuanDiego <juan.castro.p@utec.edu.pe>
2024-11-20 12:38:01 -0500

    driver code

---
+++ user/chardev-read.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUF_LEN 80
+#define CDEV_NAME "/dev/chardev"
+
+char buf[BUF_LEN + 1];
+
+int main()
+{
+	int fd, bytes_read;
+
+	fd = open(CDEV_NAME, 0, O_RDONLY);
+
+	if (fd < 0)
+	{
+		perror("Open:");
+	}
+
+	bytes_read = read(fd, buf, BUF_LEN);
+
+	if (bytes_read <= 0)
+	{
+		perror("Read: ");
+		exit(bytes_read);
+	}
+
+	printf("Read: %s from /dev/chardev\n", buf);
+}
---
+++ user/chardev-write.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define CDEV_NAME "/dev/chardev"
+#define BUF_LEN 80
+
+char buf[BUF_LEN];
+
+int main(int argc, char *argv[])
+{
+	int fd, bytes_written;
+
+	strcpy(buf, argv[1]);
+
+	fd = open(CDEV_NAME, O_CREAT | O_WRONLY, 0777);
+
+	if (fd < 0)
+	{
+		perror("Open");
+		exit(fd);
+	}
+
+	bytes_written = write(fd, buf, strlen(buf));
+
+	if (bytes_written <= 0)
+	{
+		perror("Write:");
+		exit(bytes_written);
+	}
+
+	printf("Wrote: '%s' to /dev/chardev\n", buf);
+}
